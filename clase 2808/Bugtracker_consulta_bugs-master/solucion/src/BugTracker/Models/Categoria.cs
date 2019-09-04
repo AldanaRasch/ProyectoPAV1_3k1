b86849d3c442436a6230a570466aee3340215219
ezeqsoruco <eze.soruco93@gmail.com>
2019-09-04 00:39:15 -0300

    Actualización del script de BBDD
    Se agregó tabla Categoría y algunos atriburos a la tabla Producto

---
+++ clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    class Categoria
+    {
+        int idCategoria { get; set; }
+        string nombre { get; set; }
+    }
+}
---
+++ clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/Models/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Models
+{
+    class Producto
+    {
+        int idProducto { get; set; }
+        string nombre { get; set; }
+        Categoria categoria { get; set; }
+        float precioVenta { get; set; }
+    }
+}
