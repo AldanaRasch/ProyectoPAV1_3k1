07ddf2050444acef9e61b6e248cf4a23fe912ed0
AldanaRasch <raschaldana@gmail.com>
2019-09-14 15:06:18 -0300

    Usuario

--- clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/DataAccessLayer/UsuarioDao.cs
+++ clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/DataAccessLayer/UsuarioDao.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using BugTracker.Entities;
+using System.Data;
 
 namespace BugTracker.DataAccessLayer
 {
     class UsuarioDao
     {
+        public IList<Usuario> GetAll()
+        {
+            List<Usuario> listadoBugs = new List<Usuario>();
+
+            var strSql = "SELECT id_usuario, id_perfil, usuario, password, email, estado WHERE borrado=0";
+
+            var resultadoConsulta = DBHelper.GetDBHelper().ConsultaSQL(strSql);
+
+            //foreach (DataRow row in resultadoConsulta.Rows)
+            //{
+            //    listadoBugs.Add(MappingBug(row);
+            //}
+
+            return listadoBugs;
+        }
     }
 }
--- clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/Entities/Usuario.cs
+++ clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/Entities/Usuario.cs
@@ -8,5 +8,16 @@ namespace BugTracker.Entities
 {
     class Usuario
     {
+        public int IdUsuario {get; set;}
+        public Perfil IdPerfil { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public Estado Estado { get; set; }
+
+        public override string ToString()
+        {
+            return NombreUsuario;
+        }
     }
 }
