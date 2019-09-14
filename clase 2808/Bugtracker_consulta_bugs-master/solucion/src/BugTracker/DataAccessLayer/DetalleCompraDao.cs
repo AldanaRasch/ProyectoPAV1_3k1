4abffb4b5ca7d3d3888ab6ae05e260f77c2a204f
AldanaRasch <raschaldana@gmail.com>
2019-09-14 18:38:14 -0300

    Daos

--- clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/DataAccessLayer/DetalleCompraDao.cs
+++ clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/DataAccessLayer/DetalleCompraDao.cs
@@ -1,13 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using BugTracker.Entities;
+using System.Data;
 
-namespace BugTracker.Entities
+namespace BugTracker.DataAccessLayer
 {
     class DetalleCompraDao
     {
-       
+     public IList<DetalleCompra> GetAll()
+        {
+            List<DetalleCompra> listadoBugs = new List<DetalleCompra>();
+
+            var strSQL = "SELECT codDetalle, idCompra, codProducto, cantidad, precio WHERE borrado=0";
+
+            var resultadoConsulta = DBHelper.GetDBHelper().ConsultaSQL(strSQL);
+
+            foreach( DataRow row in resultadoConsulta.Rows)
+            {
+                listadoBugs.Add(MappingBug(row));
+            };
+            return listadoBugs;
+        }
+        private DetalleCompra MappingBug(DataRow row)
+     {
+        DetalleCompra oDetalleCompra = new DetalleCompra
+        {
+            CodDetalle= Convert.ToInt32(row["codDetalle"].ToString()),
+            IdCompra= Convert.ToInt32(row["idCompra"].ToString()),
+            CodProducto= Convert.ToInt32(row["codProducto"].ToString()),
+            Cantidad= Convert.ToInt32(row["cantidad"].ToString()),
+            Precio= float.Parse(row["precio"].ToString())
+
+        };
+        return oDetalleCompra;
+     }
     }
 }
--- clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/DataAccessLayer/VentaDao.cs
+++ clase 2808/Bugtracker_consulta_bugs-master/solucion/src/BugTracker/DataAccessLayer/VentaDao.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using BugTracker.Entities;
+using System.Data;
 
 namespace BugTracker.DataAccessLayer
 {
     class VentaDao
     {
+        public IList<Venta> GetAll()
+        {
+            List<Venta> listadoBugs = new List<Venta>();
+
+            var strSQL= " SELECT nroComprobante, fecha, empleado WHERE borrado=0";
+
+            var resultadoConsulta =DBHelper.GetDBHelper().ConsultaSQL(strSQL);
+
+            foreach (DataRow row in resultadoConsulta.Rows) {
+                listadoBugs.Add(MappingBug(row));
+
+            }
+
+            return listadoBugs;
+        }
+
+        private Venta MappingBug(DataRow row)
+        {
+            Venta oVenta = new Venta
+            {
+                NroComprobante = Convert.ToInt32(row["nroComprobante"].ToString()),
+                Fecha = Convert.ToDateTime(row["fecha"].ToString()),
+                Empleado = Convert.ToInt32(row["empleado"].ToString())
+            };
+            return oVenta;
+        }
     }
 }
